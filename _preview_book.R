6f2d217a607e00edeb3248fc57e89b01c4882efb
andreas <andreas.kutka@rowsquared.com>
2023-05-04 14:58:17 +0200

    knitted Kevin's changes to introduction

--- _preview_book.R
+++ _preview_book.R
@@ -3,7 +3,7 @@
 bookdown::serve_book()
 
 #stop preview server
-servr::daemon_stop(1)
+servr::daemon_stop(4)
 
 # delete all .md files in docs (prior to git commit & push)
 unlink(list.files('docs', '\\.md$', full.names = T))
